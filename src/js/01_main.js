ab32371bdc1eaa2b169b22343f9e4eaf64fb7c09
i6rahim <i6rahim210@gmail.com>
2021-10-25 21:08:59 +0300

    add help block

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -3,10 +3,18 @@ const nav = document.getElementsByClassName('header__menu-list')[0]
 const arrows1 = document.getElementsByClassName('btn__span1')[0]
 const arrows2 = document.getElementsByClassName('btn__span2')[0]
 const arrows3 = document.getElementsByClassName('btn__span3')[0]
+const accord = document.getElementsByClassName('help__hero-item')[0]
+const accordShow = document.getElementsByClassName('help__hero-addtext')[0]
+const accord = document.querySelectorAll    
+
 burger.addEventListener('click', ()=> {
     nav.classList.toggle('active')
     arrows1.classList.toggle('active')
     arrows2.classList.toggle('active')
     arrows3.classList.toggle('active')
 
+})
+
+accord.addEventListener('click', ()=> {
+    accordShow.classList.toggle('show')
 })
