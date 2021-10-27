4bc890069197e0bea355d7c1555748d08ec192a0
i6rahim <i6rahim210@gmail.com>
2021-10-27 23:46:22 +0300

    add accordion

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -3,8 +3,10 @@ const nav     = document.getElementsByClassName('header__menu-list')[0]
 const arrows1 = document.getElementsByClassName('btn__span1')[0]
 const arrows2 = document.getElementsByClassName('btn__span2')[0]
 const arrows3 = document.getElementsByClassName('btn__span3')[0]
-const accords  = document.getElementsByClassName('help__hero-content')
-// const accordShow = document.getElementsByClassName('help__hero-content')[0]
+const accords  = document.querySelectorAll('.help__hero-item')
+const accordShow = document.querySelectorAll('.help__hero-content')
+const arrowRotate = document.querySelectorAll('.help__hero-header')
+
 burger.addEventListener('click', ()=> {
     nav.classList.toggle('active')
     arrows1.classList.toggle('active')
@@ -13,12 +15,11 @@ burger.addEventListener('click', ()=> {
 
 })
 
-// accord.addEventListener('click', ()=> {
-//     accordShow.classList.toggle('show')
-// })
+ 
+for(let i = 0; i < accords.length; i++ ){
+    accords[i].addEventListener('click', function(){
+        accordShow[i].classList.toggle('show')
+        arrowRotate[i].classList.toggle('active')
 
-// for( accords in accord ){
-//     accord.addEventListener('click', function(){
-//         this.classList.toggle('show')
-//     })
-// }
+    })
+}
